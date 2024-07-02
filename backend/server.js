22cd8236c422314d7f6ddeb70cbac91835c239d3
Juan12331 <juanxadez2@gmail.com>
2024-07-02 02:40:41 -0300

    configurando sequelize, server.js, e criando arquivos dos models (serão as tabelas)

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const sequelize = require('./config/sequelize');
+const cors = require('cors');
+const routes = require('./routes/routes');
+const usuarios = require('./models/usuarios');
+const livros = require('./models/livros');
+const autores = require('./models/autores');
+
+usuarios.sync();
+livros.sync();
+autores.sync();
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3001, () => console.log('servidor rodando na porta 3001'));
