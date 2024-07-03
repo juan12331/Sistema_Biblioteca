a0936270be22d7c016cc2be163e96820f41f01a6
Juan12331 <juanxadez2@gmail.com>
2024-07-03 11:53:29 -0300

    criando função usuarios

--- backend/controllers/usuarios.js
+++ backend/controllers/usuarios.js
@@ -1,15 +1,42 @@
 const { Op } = require('sequelize');
-const usuarios = require('../models/usuarios')
+const Usuarios = require('../models/usuarios');
 
 exports.login = async (req, res) => {
     try {
         const {email, senha} = req.body;
-        const encontrarUsuario = await usuarios.findOne({ where: {email, senha}})
+        const encontrarUsuario = await Usuarios.findOne({ where: {email, senha}})
         if (encontrarUsuario) {
             return res.send(encontrarUsuario);
         }
         return res.status(404).send('Usuario not found');
     } catch (error ) {
         return res.status(500).send('Internal Server error')
+    }
+}
+
+exports.getUsers = async (req, res) => {
+    try {
+        const { nome, email, papel} = req.query || {};
+
+        if(!nome, !email, !papel) {
+            const usuarios = Usuarios.findAll();
+            return res.send(usuarios)
+        }
+
+
+        const pesquisa = {
+            [Op.or]: [
+                nome ? { nome: { [Op.like]: `%${nome}%` } } : undefined,
+                email ? { email: { [Op.like]: `%${email}%` } } : undefined,
+                papel ? { papel: { [Op.like]: `%${papel}%` } } : undefined
+            ].filter(Boolean)
+        }
+
+        const usuarios = await Usuarios.findAll({ where: pesquisa})
+        return res.send(usuarios)
+
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send('Internal Server Error');
     }
 }
