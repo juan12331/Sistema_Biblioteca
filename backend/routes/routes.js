37716bbfa3ad2be9090c7ea12ba45836c8484831
Juan12331 <juanxadez2@gmail.com>
2024-07-04 21:11:48 -0300

    commit

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,6 +2,8 @@ const express = require('express')
 const routes = express.Router();
 const UsuariosControllers = require('../controllers/usuarios')
 
+//função dos usuarios
+
 routes.get('/usuarios', UsuariosControllers.getUsers)
 routes.get('/usuarios/:cpf', UsuariosControllers.getUsersByCpf)
 
@@ -9,5 +11,11 @@ routes.post('/login', UsuariosControllers.login)
 
 routes.post('/usuarios', UsuariosControllers.createUsuario)
 routes.delete('/usuarios/:cpf', UsuariosControllers.deleteUsuario)
+routes.put('/usaurios/:cpf', UsuariosControllers.updateUsuario)
+
+//função dos autores
+
+
+//funções dos livros
 
 module.exports = routes
