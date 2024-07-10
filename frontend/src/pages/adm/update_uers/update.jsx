060f91b759ca1d5f143ad588aeaccae8ef276d25
Juan12331 <juanxadez2@gmail.com>
2024-07-10 00:50:46 -0300

    função para formatar os numeros de telefone

--- frontend/src/pages/adm/update_uers/update.jsx
+++ frontend/src/pages/adm/update_uers/update.jsx
@@ -12,7 +12,7 @@ const update = () => {
 
   const rageEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const rageSenha = /^(?=.*[A-Z])(?=.*\d).+$/;
-  const rageCaracter =  /^.{8,}$/;
+  const rageCaracter = /^.{8,}$/;
 
   const { cpf } = useParams()
 
@@ -23,18 +23,33 @@ const update = () => {
   const [papel, setPapel] = useState('')
 
 
+  function formatPhoneNumber(phoneNumber) {
+    // Limpa todos os caracteres não numéricos
+    phoneNumber = phoneNumber.replace(/\D/g, '');
 
-  function preencher(cpf){
+    // Formatação do número de acordo com o padrão desejado
+    if (phoneNumber.length === 11) {
+        return phoneNumber.replace(/(\d{2})(\d{1})(\d{4})(\d{4})/, '($1) $2 $3-$4');
+    } else if (phoneNumber.length === 10) {
+        return phoneNumber.replace(/(\d{2})(\d{4})(\d{4})/, '($1) $2-$3');
+    } else {
+        return phoneNumber;
+    }
+}
+
+
+
+  function preencher(cpf) {
     getUsersByCpf(cpf).then(data => {
-      console.log(data)
+      const newTelefone =  formatPhoneNumber(data.telefone)
       setName(data.nome)
       setEmail(data.email)
       setPapel(data.papel)
       setSenha(data.senha)
-      setTelefone(data.telefone)
+      setTelefone(newTelefone)
 
-      
-              
+
+
     }).catch(err => console.log(err))
   }
 
@@ -42,84 +57,84 @@ const update = () => {
     const span = document.getElementById('span');
     span.textContent = message;
     console.error(message)
-}
-
-function Voltar() {
-  window.location.href = '/Adm/Users'
-}
-
-
-function Atualizar(){
-
-  if( nome == '' || email == '' || senha == '' || telefone == ''){
-    showError('preencha todos os campos')
-    return;
   }
-  if (!rageEmail.test(email)) {
-    showError('Email invalido')
-    return;
-  } if (!rageSenha.test(senha)) {
-    showError('Senha fraca, coloque numeros e letras maiusculas')
-    return;
-  } if (!rageCaracter.test(senha)) {
-    showError('senha precisa no minimo de 8 caracteres')
-    return
-  } 
-  updateUser(cpf, nome, email, senha, papel, telefone)
-  Voltar()
-}
 
-useEffect(() => {
-  preencher(cpf)
-}, [])
+  function Voltar() {
+    window.location.href = '/Adm/Users'
+  }
 
-function sair() {
-  window.location.href = "/Cadastro"
-}
+
+  function Atualizar() {
+
+    if (nome == '' || email == '' || senha == '' || telefone == '') {
+      showError('preencha todos os campos')
+      return;
+    }
+    if (!rageEmail.test(email)) {
+      showError('Email invalido')
+      return;
+    } if (!rageSenha.test(senha)) {
+      showError('Senha fraca, coloque numeros e letras maiusculas')
+      return;
+    } if (!rageCaracter.test(senha)) {
+      showError('senha precisa no minimo de 8 caracteres')
+      return
+    }
+    updateUser(cpf, nome, email, senha, papel, telefone)
+    Voltar()
+  }
+
+  useEffect(() => {
+    preencher(cpf)
+  }, [])
+
+  function sair() {
+    window.location.href = "/Cadastro"
+  }
 
 
   return (
     <>
-    <div className="header">
-      <Sidebar/>
-      <div className="text">
-                ATUALIZAR USUARIOS
-            </div>
-            <button className='button1 delete' onClick={sair} >sair</button>
+      <div className="header">
+        <Sidebar />
+        <div className="text">
+          ATUALIZAR USUARIOS
+        </div>
+        <button className='button1 delete' onClick={sair} >sair</button>
 
-    </div>
-    
-    <div>
-    <div className='Aling'>
-            <div className="row">
-                
-                
-                    <input type="text" className='InputText' value={nome} onChange={(e) => setName(e.target.value)} />
-               
-                
-                      <input type="text" className='InputText' value={telefone} onChange={(e) => setTelefone(e.target.value)}/>
-             
-            </div>
+      </div>
 
-            <div className="row">
+      <div>
+        <div className='Aling'>
+          <div className="rowTop">
 
-               
-                    <input className='InputText' variant="outlined" type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-               
-                    <input className='InputText' variant="outlined" type="text" value={senha} onChange={(e) => setSenha(e.target.value)} />
-                
+          
+            <input type="text" className='InputText' value={nome} onChange={(e) => setName(e.target.value)} />
 
-            </div>
 
-            <select value={papel} onChange={(e) => setPapel(e.target.value)} className='select'>
-                  <option value="user">user</option>
-                  <option value="adm">adm</option>
-                </select>
-            <span className='span aviso' id='span'></span>
-            <Button variant="contained" color="success" onClick={Atualizar}>Enviar</Button>
+            <input type="text" className='InputText' value={telefone} onChange={(e) => setTelefone(e.target.value)} />
+
+          </div>
+
+          <div className="rowTop">
+
+
+            <input className='InputText' variant="outlined" type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+
+            <input className='InputText' variant="outlined" type="text" value={senha} onChange={(e) => setSenha(e.target.value)} />
+
+
+          </div>  
+
+          <select value={papel} onChange={(e) => setPapel(e.target.value)} className='select'>
+            <option value="user">user</option>
+            <option value="adm">adm</option>
+          </select>
+          <span className='span aviso' id='span'></span>
+          <Button variant="contained" color="success" onClick={Atualizar}>Enviar</Button>
 
         </div>
-    </div>
+      </div>
     </>
   )
 }
