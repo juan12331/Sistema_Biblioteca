44d6ea501206164d8eba40496470b4c1ce91b734
Juan12331 <juanxadez2@gmail.com>
2024-07-11 21:20:49 -0300

    bugs fixed

--- backend/models/reclamacoes.js
+++ backend/models/reclamacoes.js
@@ -6,21 +6,13 @@ const usuarios = require('./usuarios')
 const reclamacoes = database.define('reclamacoes', {
 
     id_reclamacoes: {
-        type: Sequelize.STRING,
+        type: Sequelize.INTEGER,
         AllowNUll: false,
         primaryKey: true,
         autoIncrement: true,
     },
     assunto: {
         type: Sequelize.STRING,
-    },
-    
-    cpf_usuario: {
-        type: Sequelize.STRING,
-        references: {
-            model: usuarios,
-            key: 'cpf'
-        }
     },
     reclamacao: {
         type: Sequelize.STRING(500),
