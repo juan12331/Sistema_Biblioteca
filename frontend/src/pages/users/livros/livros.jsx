3ecf3adb931ecab6fac8a4bd3399ca9dde1680ca
Juan12331 <juanxadez2@gmail.com>
2024-07-15 09:58:10 -0300

    exportando função jsx para poder linkar nas rotas

---
+++ frontend/src/pages/users/livros/livros.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const livros = () => {
+  return (
+    <div>livros</div>
+  )
+}
+
+export default livros
---
+++ frontend/src/pages/users/reclamacoes/reclamacoes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const reclamacoes = () => {
+  return (
+    <div>reclamacoes</div>
+  )
+}
+
+export default reclamacoes
