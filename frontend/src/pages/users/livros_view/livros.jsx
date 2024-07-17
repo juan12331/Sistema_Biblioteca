a8a24d8d173fa1b4ea636db226d26ec175e5cc0a
Juan12331 <juanxadez2@gmail.com>
2024-07-16 22:00:51 -0300

    fazendo a pagina de livros view

--- frontend/src/pages/users/livros_view/livros.jsx
+++ frontend/src/pages/users/livros_view/livros.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 import Navbar from '../../../components/Navbar'
 import { useState, useEffect } from 'react'
+import './livros.css'
+import { getLivrosById, getAutoresById } from '../../../services/APIservice'
+import { useParams } from 'react-router-dom'
+
 
 
 const livros = () => {
+
+  const [image, setImage] = useState('')
+  const [nome, setNome] = useState('')
+  const [editora, setEditora] = useState('')
+  const [genero, setGenero] = useState('')
+  const [descricao, setDesc] = useState('')
+  const [quantidade, setQuantidade] = useState('')
+
+
+  const { id } = useParams()
+
+  function preencher(id) {
+    getLivrosById(id).then(data => {
+      console.log(data)
+      setImage(data.imagem)
+      setNome(data.nome)
+      setEditora(data.editora)
+      setGenero(data.genero)
+      setDesc(data.descricao)
+      setQuantidade(data.qtd_disponivel)
+      getAutoresById(data.id_autor).then(data => {
+
+      })
+
+    }).catch(error => console.error(error))
+  }
+
 
   let cpf = localStorage.getItem('cpf')
 
@@ -15,13 +46,29 @@ const livros = () => {
 
   useEffect(() => {
     verificar()
+    preencher(id)
   }, [])
 
 
   return (
-    <>
-      <Navbar/>
-    </>
+    <div className="bod">
+      <Navbar />
+
+      <div className="conectar">
+        <div className="imagem">
+          <img src={image} alt="" />
+        </div>
+
+        <div className="text">
+          <div className="titulo"> {nome} </div>
+          <span className='quantidade'> {quantidade} remanescentes </span>
+          <div className="editora"> editora: {editora} </div>
+          <div className="genero"> {genero} </div>
+          <div className="descricao"> Descrição: <br /> {descricao} </div>
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
