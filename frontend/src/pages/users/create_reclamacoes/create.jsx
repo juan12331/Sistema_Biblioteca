b48fc0f949282f3f79b23c10c74e490b00d57216
Juan12331 <juanxadez2@gmail.com>
2024-07-21 19:10:18 -0300

    estilizando pagina de fazer reclamações (fiz oq deu)

--- frontend/src/pages/users/create_reclamacoes/create.jsx
+++ frontend/src/pages/users/create_reclamacoes/create.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Navbar from '../../../components/Navbar'
 import { useState, useEffect } from 'react'
+import './create.css'
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
 
 const create = () => {
   let cpf = localStorage.getItem('cpf')
@@ -15,10 +18,39 @@ const create = () => {
     verificar()
   }, [])
 
+  const redirecionar = () => {
+    //TODO: something
+  }
+
   return (
-    <>
+    <div className="bod">
       <Navbar/>
-    </>
+
+
+<div className="corPo">
+      <div className="emailArea">
+        <div className="titulo">Fale conosco <br />
+        </div>
+      <div className="assunto">
+        <div className="assuntoText">
+          Assunto:
+        </div>
+        <input type="text" />
+      </div>
+      <div className="mensagem">
+        <div className="textareaMensagem">
+          Mensagem:
+        </div>
+        <textarea name="" id=""></textarea>
+      </div>
+
+      <div className="botao">
+      <Button variant="contained" onClick={redirecionar} > <AddIcon/> Criar</Button>
+      </div>
+      
+      </div>
+      </div>
+      </div>
   )
 }
 
